29b242197b38e544908fd983cee324afc6c132f6
Rona Krikorian <117255291+ronakrikorian@users.noreply.github.com>
2022-11-03 22:41:02 -0700

    Update interview practice.py
    changed a few things such as the customer variable and also condition if "0" is picked, I will continue to play around and upgrade this. Next I will add things for the instance the customer chooses they would like takeout instead. Once I complete that I will move on to a different project more data science related to gain more practice with python and its libraries used for data analytics and scientists.

--- interview practice.py
+++ interview practice.py
@@ -6,10 +6,8 @@ base_time = 30
 customer_first_name = input ("What is your first name?\n")
 customer_last_name = input ("What is your last name?\n")
 
-customer = str((customer_last_name, customer_first_name))
 #made into a string since could not concat tuple
-dine_in = input ("Welcome, Select 'Y' if you would like to dine-in or 'N' if you would"
-                 " like to order take out. \n")
+dine_in = input ("Welcome, " + customer_first_name + " " + customer_last_name + ". Select 'Y' if you would like to dine-in or 'N' if you would like to order take out. \n")
 if dine_in.upper() == 'Y':
     party_size = int(input("How many people are in your party?\n" ))
 
@@ -18,15 +16,19 @@ if dine_in.upper() == 'Y':
         
     else:
         print("Your table will be ready in approximately" + str(base_time+15) + " minutes.\n" )
-
-bid_yn = input ("Would you like to make a bid " + customer +" to decrease your wait time?\n"
-                "Enter '0' if you do not want to place a bid.\n" )
+        
+bid_yn = input ("Would you like to make a bid " +customer_first_name+ " " +customer_last_name+ " to decrease your wait time?\n"
+                "Enter '0' if you do not want to place a bid.\n"
+                "Enter '1' if you would like to place a bid.\n" )
 
 while bid_yn != "0":
     bid = int(input ("How much would would you like to bid for this reservation\n"))
     if bid >=party_size*100:
-        print("Your wait time is now " + str(base_time-15) + " minutes.\n")
+        print("Your wait time is now " + str(base_time-15) + " minutes.\n"
+              "Thank you! \n")
         break
     elif bid < party_size*100:
-        print ("We appologize but a larger bid is needed to decrease your wait time.\n"
-               "Would you like to place another bid?")
+        print ("\nWe appologize, but a larger bid is needed to decrease your wait time.\n")
+else: 
+    print("Thank you.\n"
+          "Your wait time is approximatly "+ str(base_time)+" minutes.")
